d37830626d4623059d6239463f044722d222f55e
Viktors Telle <viktors.telle@if.lv>
2020-06-10 07:02:52 +0300

    Add blank implementation of message outbox.

--- src/MessageOutbox/MessageOutbox.cs
+++ src/MessageOutbox/MessageOutbox.cs
@@ -1,11 +1,26 @@
-﻿using System;
+﻿
 using System.Collections.Generic;
-using System.Text;
+using System.Threading.Tasks;
 
 namespace MessageOutbox
 {
-    internal class MessageOutbox
+    internal class MessageOutbox : IMessageOutbox
     {
+        public IList<IMessage> Get()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task Save()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public interface IMessageOutbox
+    { 
+        Task Save();
         
+        IList<IMessage> Get();
     }
 }
